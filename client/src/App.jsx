e9c48d610f8ab65c8abf0b56bdfb3d576b66993d
ramwired <sreeram.codes@gmail.com>
2026-03-09 18:59:15 +0530

    feat: add React Router and basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import Navbar from "./Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./Body";
+import Profile from "./Profile";
+import Connections from "./Connections";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/connections" element={<Connections />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
